7f9997ad30d56947fd3d13d557a70db759dc6fd0
Rallve <lucas.liedgren@gmail.com>
2022-02-08 18:27:56 +0100

    fixade buggar och blev klar med koden

--- main.java
+++ main.java
@@ -3,18 +3,28 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> asci = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
+        List<String> asci = new ArrayList<>(Arrays.asList("", "============", "|\n|\n|\n|\n|\n============", "+-----------+\n|\n|\n|\n|\n|\n============", "+-----------+\n|                 |\n|\n|\n|\n|\n============", "+-----------+\n|                 |\n|                O\n|\n|\n|\n============", "+-----------+\n|                 |\n|                O\n|                 |\n|\n|\n============", "+-----------+\n|                 |\n|                O\n|               / |\n|\n|\n============", "+-----------+\n|                 |\n|                O\n|               / | \\ \n|\n|\n============", "+-----------+\n|                 |\n|                O\n|               / | \\ \n|               /\n|\n============", "+-----------+\n|                 |\n|                O\n|               / | \\ \n|               /   \\ \n|\n============"));
         List<String> ord = new ArrayList<>(Arrays.asList("multidimensional", "declaration", "metric", "subtract", "assembly", "construction", "array", "string", "variable"));
         List<String> boksGiss = new ArrayList<>(Arrays.asList(" "));
         char gissning = '\0';
         String längd = "";
         double image = 0.0;
         String rättOrd = ord.get((int) (Math.random() * ord.size()));
-        int felAnt = Integer.parseInt(JOptionPane.showInputDialog("Antal tillåtna fel?"));
-        int fel = -1;
         boolean c = false;
         boolean rätt = false;
         boolean ogiltig = false;
+        boolean vinn = false;
+        int fel = -1;
+        int felAnt = 0;
+        while (true) {
+            felAnt = Integer.parseInt(JOptionPane.showInputDialog("Antal tillåtna fel? (Max 10)"));
+            if (felAnt > 10) {
+                JOptionPane.showMessageDialog(null, "Talet du skrev in är högre än 10, vänligen skriv in ett nytt tal.");
+            }
+            else {
+                break;
+            }
+        }
         while (true) {
             for (int i = 0; i < rättOrd.length(); i++) {
                 for (int n = 0; n < boksGiss.size(); n++) {
@@ -28,14 +38,23 @@ public class Main {
                     }
                 if (c == false) {
                     längd += "_ ";
+                    vinn = false;
                     }
                 c = false;
+            }
+            if (vinn == true) {
+                JOptionPane.showMessageDialog(null, asci.get((int) image) + "\n" + längd + "\n" + boksGiss + "\nDu vann!");
+                break;
             }
             if (rätt == false) {
                 fel += 1;
             }
             rätt = false;
             image = ((double) fel / felAnt) * 10;
+            if (image >= 10) {
+                JOptionPane.showMessageDialog(null, asci.get((int) 10) + "\n" + längd + "\n" + boksGiss + "\nDu förlorade :(");
+                break;
+            }
             while (true) {
                 gissning = JOptionPane.showInputDialog(asci.get((int) image) + "\n" + längd + "\n" + boksGiss + "\nGissa på en bokstav.").charAt(0);
                 for (int i = 0; i < boksGiss.size(); i++) {
@@ -52,10 +71,8 @@ public class Main {
                 }
             }
             längd = "";
+            vinn = true;
             boksGiss.add(String.valueOf(gissning));
         }
     }
 }
-/* Kvar att göra:
-Funktionalitet för att gissa på hela ordet.
-ASCII bilder. */
